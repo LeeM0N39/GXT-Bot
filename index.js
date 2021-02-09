04c47753e23c5ac01e30a1bb2754552f16ee1caf
LeeM0N39 <67191861+LeeM0N39@users.noreply.github.com>
2021-02-09 21:02:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const{ Client } = require('discord.js')
-
-const client = new Client();
-
-client.on('ready', () => {
-    console.log(`${client.user.username} has logged on`)
+const {Collection, Client, Discord} = require('discord.js')
+const fs = require('fs')
+const client = new Client({
+    disableEveryone: true
 })
-
-client.login('NzcyNDI4NzE2MTA1NzkzNTg3.X56iTA.-8aVQ9V4abo0pvY7Z_kpl-30V0k')
+const config = require('./config.json')
+const prefix = config.prefix
+const token = config.token
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+}); 
+client.on('ready', () => {
+    client.user.setActivity('Patriche Anisoara on PornHub 2e/Video')
+    console.log(`${client.user.username} ✅`)
+})
+client.on('message', async message =>{
+    if(message.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(!message.guild) return;
+    if(!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if(cmd.length == 0 ) return;
+    let command = client.commands.get(cmd)
+    if(!command) command = client.commands.get(client.aliases.get(cmd));
+    if(command) command.run(client, message, args) 
+})
+client.login(token)
